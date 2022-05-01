2d54183105e3acacaf4e230f4d888d168db29571
jchy <recjaswantcse@gmail.com>
2022-05-01 13:04:57 +0530

    feat : print 1 to 5 at an regular interval of 1 seconds

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
 // Closure
 // Function along with it's lexical scope is called closure
 
-function x(){
-    var a=7;
-   function y(){
-        console.log(a);
-    }
-    a=100;
-    return y;
-}
-x()();
+// function x(){
+//     var a=7;
+//    function y(){
+//         console.log(a);
+//     }
+//     a=100;
+//     return y;
+// }
+// x()();
 
-function fun(){
-    var a=7;
-   return function sun(){
-        console.log(a);
+// function fun(){
+//     var a=7;
+//    return function sun(){
+//         console.log(a);
+//     }
+//     a=100;
+// }
+// fun()();
+
+printOneToFive = function (){
+    for(var i=1; i<=5;i++){
+        setTimeout(()=>{
+            console.log("Unintended output : ",i);
+        },i*1000)
     }
-    a=100;
 }
-fun()();
+printOneToFive()
+
+printOneToFiveSoln = function (){
+    for(let i=1; i<=5;i++){
+        setTimeout(()=>{
+            console.log("expected output : ",i);
+        },i*1000)
+    }
+}
+printOneToFiveSoln()
